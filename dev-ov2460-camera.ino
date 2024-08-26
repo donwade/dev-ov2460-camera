aa0fb0b45aea691ec8f74caf0e8e263ca0373d0b
don wade <don_wade@rogers.com>
2024-08-26 12:48:31 -0400

    %1

--- dev-ov2460-camera.ino
+++ dev-ov2460-camera.ino
@@ -14,6 +14,8 @@
 
 void setup() {
   Serial.begin(115200);
+  delay(2000);
+
   Serial.setDebugOutput(false);
   Serial.println();
   pinMode(BUTTON_PIN, INPUT_PULLUP);
@@ -27,8 +29,8 @@ void setup() {
   }
   else{
     ws2812SetColor(1);
-    return;
   }
+  delay(100);
 }
 
 void loop() {
@@ -37,15 +39,33 @@ void loop() {
     if(digitalRead(BUTTON_PIN)==LOW){
 	  	ws2812SetColor(3);
       while(digitalRead(BUTTON_PIN)==LOW);
+
       camera_fb_t * fb = NULL;
       fb = esp_camera_fb_get();
+
+      printf("sizeof size_t = %d\n", sizeof(size_t));
+      printf("fb->buff = %p \n", fb->buf);
+      printf("fb->format = %d \n", fb->format);
+      printf("fb->height = %d \n", fb->height);
+      printf("fb->len = %d \n", fb->len);   // length of buffer.
+      printf("fb->time = %d \n", fb->timestamp);
+
+      assert ( fb->height);
+
+      int width = fb->len / fb->height;
+      printf("width = %d (fb->len / fb->fb_height)\n", width);
+
+      int one_element = fb->len / (width * fb->height);
+      printf("one_element size = %d\n", one_element);
       if (fb != NULL) {
+/*
         int photo_index = readFileNum(SD_MMC, "/camera");
         if(photo_index!=-1)
         {
           String path = "/camera/" + String(photo_index) +".jpg";
           writejpg(SD_MMC, path.c_str(), fb->buf, fb->len);
         }
+*/
         esp_camera_fb_return(fb);
       }
       else {
@@ -77,16 +97,25 @@ int cameraSetup(void) {
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
-  config.frame_size = FRAMESIZE_UXGA;
-  config.pixel_format = PIXFORMAT_JPEG; // for streaming
+
+  config.frame_size = FRAMESIZE_UXGA;  // 1600x1200
+  //config.frame_size = FRAMESIZE_SXGA;   // 1280x1024
+  //config.frame_size = FRAMESIZE_QVGA;   // 320x240
+
+  //config.pixel_format = PIXFORMAT_JPEG; // for streaming
+  //config.pixel_format = PIXFORMAT_GRAYSCALE;    //
+  //config.pixel_format = PIXFORMAT_RGB555;    //
+  config.pixel_format = PIXFORMAT_RGB565;    //
+  //config.pixel_format = PIXFORMAT_RAW;    //
   config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
   config.fb_location = CAMERA_FB_IN_PSRAM;
   config.jpeg_quality = 12;
   config.fb_count = 1;
-  
+
   // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
   // for larger pre-allocated frame buffer.
   if(psramFound()){
+    Serial.println ("PSRAM READY --------------");
     config.jpeg_quality = 10;
     config.fb_count = 2;
     config.grab_mode = CAMERA_GRAB_LATEST;
@@ -108,6 +137,19 @@ int cameraSetup(void) {
   s->set_vflip(s, 1); // flip it back
   s->set_brightness(s, 1); // up the brightness just a bit
   s->set_saturation(s, 0); // lower the saturation
+
+  static char crlf = 0;
+  for (int reg = 0; reg < 0x200; reg++)
+  {
+    int value;
+    value = s->get_reg(s, reg, 0xFF);
+
+    if (value == 0) continue;
+
+    if (!(crlf++ & 0x3)) Serial.println();
+    printf("0x%03X = 0x%02X\n", reg, s->get_reg(s, reg, 0xFF));
+  }
+
 
   Serial.println("Camera configuration complete!");
   return 1;
